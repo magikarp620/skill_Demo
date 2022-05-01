24904fb563756845beaa64f282c7fa78666e602a
magikarp620 <rogerlin392@gmail.com>
2022-05-01 13:12:48 -0700

    Update
    added method
    and junit test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,4 +1,7 @@
 public class SkillDemo{
 
+    public static int minus(int a, int b){
+        return a-b;
+    }
 
 }
---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+
+    @Test
+    public void subtraction(){
+        assertEquals(2, SkillDemo.minus(4,3));
+    }
+}
